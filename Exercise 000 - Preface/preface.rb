45399fd9884c8b0cdee817eec2a0b14c1d7387cd
Roland Burrows <rolandnburrows@gmail.com>
2015-08-16 21:19:31 -0600

    Preface completed

---
+++ Exercise 000 - Preface/preface.rb
@@ -0,0 +1,8 @@
+# "Hard" way because it uses a technique called instruction.
+
+# - Instruction: do a sequence of controlled exercises designed to build a
+#   skill through repetition. Works well to help beginners understand more
+#   complex topics.
+
+# - Will build and establish skills through techniques like practice and
+#   memorization, then applying them to increasingly difficult problems.
