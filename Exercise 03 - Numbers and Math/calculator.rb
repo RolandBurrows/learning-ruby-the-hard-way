e9423f8d4300594620ce03c76086d5dab179fde5
Roland Burrows <rolandnburrows@gmail.com>
2015-09-01 02:06:08 -0600

    Exercise 3 study drill tweaks

--- Exercise 03 - Numbers and Math/calculator.rb
+++ Exercise 03 - Numbers and Math/calculator.rb
@@ -1,10 +1,12 @@
 # http://learnrubythehardway.org/book/ex3.html
 
-# Study Drill #3
+# Study Drill 3.
+# Find something you need to calculate and write a new .rb file that does it.
 
+# Chain Calculator script
 puts "|"
 puts "|  Input the first num you wish to calculate against."
-@response1 = gets.chomp.to_f   # The 'gets' method returns the user's input, the 'chomp' removes the newline, and to_f converts to float number
+@response1 = gets.chomp.to_f   # The 'gets' method returns the user's input, the 'chomp' removes the newline
 puts "|  Initializing calculations with (#{@response1})."
 puts "|"
 @total = @response1
@@ -20,7 +22,7 @@ until @response2 == "=" do
 	@operand = @response2[/(?<=^.{1}).*/].to_f
 	# puts @operand   # Diagnostic
 	@result = @total.send(@operator, @operand)   # Mathematically operates the operand on the response
-	# puts @result   #Diagnostic
+	# puts @result   # Diagnostic
 	puts "|"
 	puts "|  (#{@total}) #{@operator} (#{@operand}) = (#{@result})"
 	@total = @result
@@ -30,3 +32,32 @@ until @response2 == "=" do
 end
 
 puts "|  Final value = (#{@total})"
+
+
+# Sample output:
+
+# $ ruby calculator.rb
+# |
+# |  Input the first num you wish to calculate against.
+# 1
+# |  Initializing calculations with (1.0).
+# |
+# |  Perform next operation. Ex: '+2'. Use '=' to finalize calculation.
+# +2
+# |
+# |  (1.0) + (2.0) = (3.0)
+# |
+# |  Perform next operation. Ex: '+2'. Use '=' to finalize calculation.
+# +3
+# |
+# |  (3.0) + (3.0) = (6.0)
+# |
+# |  Perform next operation. Ex: '+2'. Use '=' to finalize calculation.
+# -4
+# |
+# |  (6.0) - (4.0) = (2.0)
+# |
+# |  Perform next operation. Ex: '+2'. Use '=' to finalize calculation.
+# =
+# |  Final value = (2.0)
+# $
--- README.md
+++ README.md
@@ -9,5 +9,5 @@ Archived notes of my Ruby learnings via http://learnrubythehardway.org/book/.
 | Setup       | 8-19-2015     | 8-19-2015     | 0.25          |              | [Notes](https://github.com/RolandBurrows/learning-ruby-the-hard-way/tree/master/Exercise%200%20-%20The%20Setup) |
 | Exercise 01 | 8-19-2015     | 8-19-2015     | 0.50          |              | [Notes](https://github.com/RolandBurrows/learning-ruby-the-hard-way/tree/master/Exercise%201%20-%20A%20Good%20First%20Program) |
 | Exercise 02 | 8-23-2015     | 8-23-2015     | 0.25                        || [Notes](https://github.com/RolandBurrows/learning-ruby-the-hard-way/tree/master/Exercise%2002%20-%20Comments%20and%20Pound%20Characters) |
-| Exercise 03 | 8-28-2015     | 9-01-2015     | 0.50          | 2.00         | [Notes](https://github.com/RolandBurrows/learning-ruby-the-hard-way/tree/master/Exercise%2003%20-%20Numbers%20and%20Math) |
-| **Totals**  | **8-16-2015** | **Ongoing**   | **2.50**      | **2.00**     | -    |
+| Exercise 03 | 8-28-2015     | 9-01-2015     | 0.50          | 2.25         | [Notes](https://github.com/RolandBurrows/learning-ruby-the-hard-way/tree/master/Exercise%2003%20-%20Numbers%20and%20Math) |
+| **Totals**  | **8-16-2015** | **Ongoing**   | **2.50**      | **2.25**     | -    |
