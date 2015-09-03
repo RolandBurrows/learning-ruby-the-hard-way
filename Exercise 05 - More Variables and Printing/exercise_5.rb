1d57609091bce5858d9fa89b92a8dc08e9612784
Roland Burrows <rolandnburrows@gmail.com>
2015-09-03 00:16:05 -0600

    Exercise 5 completed

--- Exercise 05 - More Variables and Printing/exercise_5.rb
+++ Exercise 05 - More Variables and Printing/exercise_5.rb
@@ -1,5 +1,10 @@
 # http://learnrubythehardway.org/book/ex5.html
 
+def convertInchesToCentimeters(num)
+  cm = (num * 2.54)
+  return cm
+end
+
 def convertPoundsToKilograms(num)
   lbs = num
   kilo = (lbs * 0.453592)
@@ -9,7 +14,7 @@ end
 my_name = 'Roland N. Burrows'
 my_age = 29 # not a lie in 2015
 my_height = 77 # inches
-my_height_metric = 
+my_height_metric = convertInchesToCentimeters(my_height)
 my_weight = 280 # lbs
 my_weight_metric = convertPoundsToKilograms(my_weight)
 my_eyes = 'Blue'
@@ -18,11 +23,12 @@ my_hair = 'Red'
 
 puts "Let's talk about (#{my_name})."
 puts "He's (#{my_height}) inches tall."
+puts "That's (#{my_height_metric}) centimeters!"
 puts "He's (#{my_weight}) pounds heavy."
 puts "That's (#{my_weight_metric}) kilograms!"
 puts "Actually, that's pretty heavy."
 puts "He's got (#{my_eyes}) eyes and (#{my_hair}) hair."
-puts "His teeth are usually (#{my_teeth}), depending on the tea."
+puts "His teeth are usually (#{my_teeth}), depending on the tea he drinks."
 
 # This line is tricky - try to get it exactly right.
 puts "If I add (#{my_age}), (#{my_height}), and (#{my_weight}), I get (#{my_age + my_height + my_weight})."
