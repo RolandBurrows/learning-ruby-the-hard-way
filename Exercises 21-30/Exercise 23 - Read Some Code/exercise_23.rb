2ee4e652c86c30582840ee5b725d33e80025a53a
Roland Burrows <rolandnburrows@gmail.com>
2015-09-19 10:57:48 -0600

    Exercise 23 research topic 1 completed

--- Exercises 21-30/Exercise 23 - Read Some Code/exercise_23.rb
+++ Exercises 21-30/Exercise 23 - Read Some Code/exercise_23.rb
@@ -5,3 +5,19 @@
 # Two topics of research interest to me:
 # - Git vs Mercurial (only ever heard of mercurial)
 # - Hosted Distributed Version Control Systems options (github, bitbucket, etc.)
+
+
+### Compare Git + Mercurial ###
+
+# As of 9/19/2015:
+# This is an excellent Git tutorial: http://rogerdudler.github.io/git-guide/
+# This is an excellent Mercurial tutorial: http://hginit.com/
+# And this is a slightly dated, but solid comparison: https://importantshock.wordpress.com/2008/08/07/git-vs-mercurial/
+
+# Summary:
+# Git pros: Flexible, speedy, scales
+# Git cons: Complex, esoteric
+# Mercurial pros: Simple, works-out-of-the-box
+# Mercurial concs: Seemingly inflexible, doesn't scale well
+# The average person working on simple projects, don't care about the slight differences. Use whatever you prefer.
+# For a larger org with many developers and projects, Git is more effective.
