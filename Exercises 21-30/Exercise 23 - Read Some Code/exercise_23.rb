21d466cd077007819c6f8d0d55f7e3b7ae921370
Roland Burrows <rolandnburrows@gmail.com>
2015-09-19 10:44:44 -0600

    Exercise 23 init

---
+++ Exercises 21-30/Exercise 23 - Read Some Code/exercise_23.rb
@@ -0,0 +1,7 @@
+# http://learnrubythehardway.org/book/ex23.html
+
+# I read code every day for professional work, sooo No. But! The point of this exercise is research.
+
+# Two topics of research interest to me:
+# - Git vs Mercurial (only ever heard of mercurial)
+# - Hosted Distributed Version Control Systems options (github, bitbucket, etc.)
