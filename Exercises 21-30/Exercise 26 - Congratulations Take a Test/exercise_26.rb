a5be27dc3a2c9cf717a3382f892f4e0ab77f7e4c
Roland Burrows <rolandnburrows@gmail.com>
2015-09-21 23:44:15 -0600

    Exercise 26 completed

--- Exercises 21-30/Exercise 26 - Congratulations Take a Test/exercise_26.rb
+++ Exercises 21-30/Exercise 26 - Congratulations Take a Test/exercise_26.rb
@@ -1,51 +1,51 @@
 # http://learnrubythehardway.org/book/ex26.html
 
-module Ex2
+module Ex25
 
   # This function will break up words for us.
-  def Ex25.brak_words(stuff
+  def Ex25.break_words(stuff)
     words = stuff.split(' ')
-    return word
+    return words
   end
 
   # Sorts the words.
-  def Ex25.sortwords(words)
+  def Ex25.sort_words(words)
     return words.sort
   end
 
   # Prints the first word after popping it off.
-  df Ex25.print_first_word(words)
+  def Ex25.print_first_word(words)
     word = words.pop(1)
-    puts wor
+    puts word
   end
 
   # Prints the last word after popping it off.
-  def Ex25:print_last_word(words)
+  def Ex25.print_last_word(words)
     word = words.pop
-    put word
+    puts word
   end
 
   # Takes in a full sentence and returns the sorted words.
   def Ex25.sort_sentence(sentence)
     words = Ex25.break_words(sentence)
     return Ex25.sort_words(words)
-  ed
+  end
 
   # Prints the first and last words of the sentence.
-  def Ex25.print_first_and_last(sentence
-    words = Ex25.break_words(sentenc)
-    Ex25.print_first_wrd(word)
+  def Ex25.print_first_and_last(sentence)
+    words = Ex25.break_words(sentence)
+    Ex25.print_first_word(words)
     Ex25.print_last_word(words)
   end
 
   # Sorts the words then prints the first and last one.
   def Ex25.print_first_and_last_sorted(sentence)
     words = Ex25.sort_sentence(sentence)
-    Ex25.print_fist_word(words)
+    Ex25.print_first_word(words)
     Ex25.print_last_word(words)
   end
 
-
+end
 
 puts "Let's practice everything."
 puts 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
@@ -57,39 +57,67 @@ cannot discern \n the needs of love
 nor comprehend passion from intuition
 and requires an explanation
 \n\t\twhere there is none.
-ENDED
+END
 
 puts "--------------"
 puts poem
 puts "--------------"
 
 
-five = 10 - 2  3 - 6
-puts "This should be five: #{five"
+five = 10 - 2 + 3 - 6
+puts "This should be five: #{five}"
 
 def secret_formula(started)
-  jelly_bens = started * 500
+  jelly_beans = started * 500
   jars = jelly_beans / 1000
-  crate = jars / 100
+  crates = jars / 100
   return jelly_beans, jars, crates
 end
 
 
 start_point = 10000
-beans, jars crates = secret_formula(start_point)
+jelly_beans, jars, crates = secret_formula(start_point)
 
 puts "With a starting point of: #{start_point}"
-puts "We'd have #{beans beans, #{jars} jars, and #{crates} crates."
+puts "We'd have #{jelly_beans} beans, #{jars} jars, and #{crates} crates."
 
 start_point = start_point / 10
 
 sentence = "All good things come to those who wait."
 words = Ex25.break_words(sentence)
 sorted_words = Ex25.sort_words(words)
-Ex25.print_first_word(wrds)
-Ex25.print_last_word words)
-Ex25.print_first_word(sort_words)
+Ex25.print_first_word(words)
+Ex25.print_last_word(words)
+Ex25.print_first_word(sorted_words)
 Ex25.print_last_word(sorted_words)
-sorted_words = Ex25.sort_sentenc(sentence)
+sorted_words = Ex25.sort_sentence(sentence)
 Ex25.print_first_and_last(sentence)
-Ex25:print_first_and_last_sorted(sentence)
+Ex25.print_first_and_last_sorted(sentence)
+
+# Sublime Text's color formatting really made this easy, woo!
+
+# Output:
+
+# Let's practice everything.
+# You'd need to know 'bout escapes with \ that do \n newlines and \t tabs.
+# --------------
+#         The lovely world
+# with logic so firmly planted
+# cannot discern
+#  the needs of love
+# nor comprehend passion from intuition
+# and requires an explanation
+
+#                 where there is none.
+# --------------
+# This should be five: 5
+# With a starting point of: 10000
+# We'd have 5000000 beans, 5000 jars, and 50 crates.
+# wait.
+# who
+# who
+# wait.
+# wait.
+# who
+# who
+# wait.
