9239fea2c358e88aaa36a59661fcf056a879fb11
Roland Burrows <rolandnburrows@gmail.com>
2015-09-27 14:28:20 -0600

    Exercise 36 init

---
+++ Exercises 31-40/Exercise 36 - Designing and Debugging/exercise_36.rb
@@ -0,0 +1,9 @@
+# http://learnrubythehardway.org/book/ex36.html
+
+# Game Design Notes:
+# Puzzler with no dangers, per se. Only ways out are to starve to death on purpose or after X turns, or to win.
+# Time-machine based with 3 rooms:
+# - Central time core
+# - Numbers Room (1, 2, 3), activate by entering proper numbers
+# - Letter Room (A, B, C), activate by entering proper words
+# Rooms can only be activated with correct puzzle entry AND correct time of day.
