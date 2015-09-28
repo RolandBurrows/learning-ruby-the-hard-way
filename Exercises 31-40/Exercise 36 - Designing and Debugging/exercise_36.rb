fe5ae01b45eb958fd9d4dc7a9c844245c1b8378e
Roland Burrows <rolandnburrows@gmail.com>
2015-09-27 23:08:18 -0600

    Exercise 36 moar

--- Exercises 31-40/Exercise 36 - Designing and Debugging/exercise_36.rb
+++ Exercises 31-40/Exercise 36 - Designing and Debugging/exercise_36.rb
@@ -8,8 +8,10 @@
 # - Letter Room (A, B, C), activate by entering proper words
 # Rooms can only be activated with correct puzzle entry AND correct time of day.
 
-$sykles = 100
-  
+$sykles = 101
+$time_dial = "present"
+$time_portal = "inactive"
+
 def story_text(exposition)
   puts ("| #{exposition}")
 end
@@ -27,13 +29,8 @@ def location_text(locale)
   story_text(("-" * ((locale.length)+10)))
 end
 
-def sykles_remaining
-  command_text("Sykles until the time wizard returns: (#{$sykles}).")
-  sykles_check
-end
-
 def sykles_check
-  if $sykles == 0
+  if $sykles <= 0
     story_text("Kronus has returned to his laboratory through the time portal with a fully recharged Rod of Ages.")
     story_text("He instantly detects you outside of your cage and teleports behind you, most displeased.")
     story_text("Your body collapses into a micro-singularity and is deposited into the tower's furnace.")
@@ -41,6 +38,42 @@ def sykles_check
     exit(0)
   end
 end
+
+def sykles_remaining
+  $sykles -= 1
+  command_text("Sykles until the time wizard returns: (#{$sykles}).")
+  sykles_check
+end
+
+def user_continue
+  command_text("Press ENTER to continue.")
+  $stdin.gets
+end
+
+# def action_or_move(action_text, action_word, action_action, move_text, move_word, move_action)
+#   command_text("Do you wish to #{action_text}, or #{move_text}?")
+#   response = $stdin.gets.chomp
+#   if response.include? (action_word)
+#     action_action
+#   elsif response.include? (move_word)
+#     move_action
+#   else
+#     story_text("There's no time to goof around! The wizard will returning!")
+#     sykles_remaining
+#   end
+# end
+
+### Actions
+
+# def action_activate_time_dial
+#   story_text("You activate the time dial.")
+# end
+
+# def action_time_dial_options
+#   story_text("Here are the time dial options:")
+# end 
+
+
 
 def intro
   command_text("GAME START")
@@ -53,6 +86,7 @@ def intro
   story_text("But you awoke hazily, feeling damp stone and cold iron, already aware that there were other cells nearby.")
   story_text("Imprisoned in his temporal laboratory, you were one of many 'samples' stolen from time.")
   story_text("However, you were the only human being to be captured, and you've been watching endlessly for a way out...")
+  user_continue
 end
 
 def cellblock
@@ -65,17 +99,29 @@ def cellblock
   story_text("He raises the Rod, which begins to glow... but sputters out into darkness.")
   story_text("Kronus devishly grins, 'You are lucky, little imps. My precious must now feed on temporal energies. But afterwards...'")
   command_text("Kronus exits.")
-  story_text("Time to act! It will take him at least #{$sykles} to recharge the Rod from the deepest well of the timestream.")
+  story_text("Time to act! It will take him at least #{$sykles} sykles to recharge the Rod from the deepest well of the timestream.")
   story_text("Having seen him draw the Rune of Opening on the gateway locks, you open your cage and exit the cellblock.")
+  sykles_remaining
+  user_continue
+end
+
+def room_portal_intro
+  story_text("You stand in the circular room that houses the heart of the tower: The Time Portal.")
+  story_text("You've seen the mechanism operated before - it's controlled mentally.")
+  story_text("Unfortunately, it requires three locks to be opened before the portal itself can be opened.")
+  story_text("For Kronus, this is trivial. He can simply control time and space. But you must use the time dial on the portal control.")
+  user_continue
 end
 
 def room_portal
-  location_text("The portal room")
+  location_text("The portal room, the #{$time_dial}")
+  command_text("The time portal is: (#{$time_portal}).")
+  story_text("Innumerable doors line the perimeter, but you know the three needed.")
 end
 
 ### Begin game sequence
 
 intro
-sykles_remaining
 cellblock
+room_portal_intro
 room_portal
