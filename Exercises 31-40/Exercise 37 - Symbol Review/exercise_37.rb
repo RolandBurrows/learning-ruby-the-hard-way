3a8f6c8b8f68f8f58067e9d46f05ddf0ac444d58
Roland Burrows <rolandnburrows@gmail.com>
2015-09-29 01:38:01 -0600

    Exercise 37 basics

---
+++ Exercises 31-40/Exercise 37 - Symbol Review/exercise_37.rb
@@ -0,0 +1,7 @@
+# http://learnrubythehardway.org/book/ex37.html
+
+topics = ["Symbols", "Data Types", "String Escape Sequences", "Operators"]
+
+topics.each do |topic|
+  puts "#{topic} reviewed!"
+end
