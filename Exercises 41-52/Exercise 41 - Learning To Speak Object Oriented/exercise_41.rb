7191581b73709a21fe3f80727553b16fe8f3746b
Roland Burrows <rolandnburrows@gmail.com>
2015-10-08 23:16:10 -0600

    Exercise 41 completed

--- Exercises 41-52/Exercise 41 - Learning To Speak Object Oriented/exercise_41.rb
+++ Exercises 41-52/Exercise 41 - Learning To Speak Object Oriented/exercise_41.rb
@@ -1,1 +1,33 @@
 # http://learnrubythehardway.org/book/ex41.html
+
+# Reading Test
+
+# class Competition
+#         def initialize(bag, dinosaurs)
+#         end
+# end
+
+# > class Comptetition has-a initialize that takes bag and dinosaurs as parameters
+
+# ANSWER:  class Competition has-a initialize that takes bag, dinosaurs parameters.
+
+# cable.bait = 'balloon'
+
+# > from cable get the bait attribute and set it to ballon
+
+# ANSWER:  From cable get the bait attribute and set it to 'balloon'.
+
+# crib = Dog.new()
+
+# > set crib to an instance of Dog
+
+# ANSWER:  Set crib to an instance of class Dog.
+
+# class Bucket < Disease
+# end
+
+# > class Bucket is-a Disease
+
+# ANSWER:  Make a class named Bucket that is-a Disease.
+
+# etc, etc.
