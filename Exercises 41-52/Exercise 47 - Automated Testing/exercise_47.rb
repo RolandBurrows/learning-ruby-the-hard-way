34972e4b09f5a9d266daedbd9f3bd4401b322431
Roland Burrows <rolandnburrows@gmail.com>
2015-10-14 07:40:47 -0600

    Exercise 47 generate folder structure

--- Exercises 41-52/Exercise 47 - Automated Testing/exercise_47.rb
+++ Exercises 41-52/Exercise 47 - Automated Testing/exercise_47.rb
@@ -1,2 +1,8 @@
 # http://learnrubythehardway.org/book/ex47.html
 
+# Windows powershell command to rename skeleton files:
+# Get-ChildItem -File -Recurse | % { Rename-Item -Path $_.PSPath -NewName $_.Name.replace("NAME","ex47")}
+
+# Yet another reason that developing on a mac is easier
+
+
