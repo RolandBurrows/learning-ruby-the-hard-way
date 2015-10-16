cee5cdc97c5d814fd56e0518008ba0a91bd15bd8
Roland Burrows <rolandnburrows@gmail.com>
2015-10-15 21:44:35 -0600

    Exercise 48 init

---
+++ Exercises 41-52/Exercise 48 - Advanced User Input/exercise_48.rb
@@ -0,0 +1,8 @@
+# http://learnrubythehardway.org/book/ex48.html
+
+# Intro:
+# In past games you handled the user's input by simply expecting set strings. If the user typed
+# "run", and excatly "run", then the game worked.
+
+# Me: This had previously occurred to me.
+
